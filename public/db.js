29c928eab8b1bc40bb5daedf4086c927a123eec0
CJ Dodge <hitmancjack@yahoo.com>
2020-12-08 17:37:06 -0800

    created db.js and service-worker.js files

---
+++ public/db.js
@@ -0,0 +1,65 @@
+let db;
+
+//creates a db request for a "budget" database
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    // create object store "pending"
+   const db = event.target.result;
+   db.createObjectStore("pending", { autoIncrement: true });
+ };
+ 
+ request.onsuccess = function(event) {
+   db = event.target.result;
+ 
+   //checks if app is online and if so check db
+   if (navigator.onLine) {
+     checkDatabase();
+   }
+ };
+ 
+ request.onerror = function(event) {
+   console.log("Error " + event.target.errorCode);
+ };
+ 
+ function saveInput(input) {
+   //Creates a transaction for pending db
+   const transaction = db.transaction(["pending"], "readwrite");
+ 
+   //Access the pending object store
+   const store = transaction.objectStore("pending");
+ 
+   //Add input to store
+   store.add(input);
+ }
+
+ function checkDatabase() {
+    //Opens transaction on your pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    //Access pending object store
+    const store = transaction.objectStore("pending");
+    //Get all inputs from store and set to variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                //opens transaction on pending db
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                //Accesses pending object store
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        }
+    };
+ }
